77c2034b768ee444f131f6d4f7dae02bd6e9225e
Piyush Sonkar <piyushsonkar2201@gmail.com>
2025-09-23 10:16:37 +0000

    File changes

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,10 @@
+// It is safe to store this configuration publicly.
+// See https://firebase.google.com/docs/projects/manage-firebase-apps#get-config-obj
+export const firebaseConfig = {
+  projectId: 'studio-452461374-acd04',
+  appId: '1:695238948298:web:feceb8b6d0a94ff0c68b33',
+  apiKey: 'AIzaSyBwRMsX08dp6vvgfybIeOToCYtEcxP-l6M',
+  authDomain: 'studio-452461374-acd04.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '695238948298',
+};
